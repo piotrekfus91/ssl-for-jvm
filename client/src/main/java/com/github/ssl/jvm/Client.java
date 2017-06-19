e6cff0cf79c27f4e09ef5538aea72180e49f6ee7
Piotr Fus <piotrek.fus.91@gmail.com>
2017-06-19 08:54:33 +0200

    ensure client certificate

--- client/src/main/java/com/github/ssl/jvm/Client.java
+++ client/src/main/java/com/github/ssl/jvm/Client.java
@@ -8,19 +8,23 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.ssl.SSLContexts;
 
 import javax.net.ssl.SSLContext;
+import java.io.FileInputStream;
 import java.io.InputStream;
 import java.security.KeyStore;
 
 public class Client {
     public static void main(String[] args) throws Exception {
         KeyStore keyStore = KeyStore.getInstance("PKCS12");
-        keyStore.load(Client.class.getResourceAsStream("server.p12"), "changeit".toCharArray());
+        keyStore.load(new FileInputStream(args[0]), getPassword(args));
         SSLContext sslContext = SSLContexts.custom().loadKeyMaterial(keyStore, null).build();
         HttpClient httpClient = HttpClients.custom().setSslcontext(sslContext).build();
         HttpResponse response = httpClient.execute(new HttpGet("https://localhost:8443"));
         InputStream content = response.getEntity().getContent();
         String responseText = IOUtils.toString(content, "UTF-8");
         System.out.println(responseText);
+    }
 
+    private static char[] getPassword(String[] args) {
+        return args.length > 1 ? args[1].toCharArray() : "".toCharArray();
     }
 }
--- server/src/main/java/com/github/ssl/jvm/Server.java
+++ server/src/main/java/com/github/ssl/jvm/Server.java
@@ -2,15 +2,41 @@ package com.github.ssl.jvm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @SpringBootApplication
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @RestController
-public class Server {
+public class Server extends WebSecurityConfigurerAdapter {
     @GetMapping
-    public String root() {
-        return "It works!";
+    public String root(Principal principal) {
+        return "You are mostly welcome here, " + principal.getName() + "!";
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .x509()
+                .subjectPrincipalRegex("CN=(.*?)(?:,|$)")
+                .userDetailsService(buildUserDetailsService());
+    }
+
+    private UserDetailsService buildUserDetailsService() {
+        return username -> new User(username, "", AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER"));
     }
 
     public static void main(String[] args) {
